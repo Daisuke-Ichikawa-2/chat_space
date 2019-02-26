3898df5fae337e16766337a2f387c88520081923
Daisuke-Ichikawa-2 <amethyst.rook@gmail.com>
2019-02-26 19:07:58 +0900

    機能更新、実装
    ・グループ管理
    ・メッセージ機能送受信

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      root 'chat_space#index'
-      get 'chat_space' => 'chat_space#index'
-      resources :users
-      # resources :users, only: [:edit, :update]
-      resources :groups
+    root 'groups#index'
+    resources :users
+    # resources :users, only: [:edit, :update]
+    resources :groups do
+      resources :messages
+    end
 end
