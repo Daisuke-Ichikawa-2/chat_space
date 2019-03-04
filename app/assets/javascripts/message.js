bc821c6a83a82d7a099c5cb20131f48ddecba988
Daisuke-Ichikawa-2 <amethyst.rook@gmail.com>
2019-03-04 12:46:12 +0900

    非同期通信処理の実装
    ・コンテント送信についての非同期化
    
    2019/03/04 12:30完了しました。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,52 @@
+$(function() {
+  function buildHTML(mes){
+    var html = `
+                  <div class = "contents__main-contents__body__space3" ></div>
+                  <div class = "contents__main-contents__body__text" >
+                  <div class = "contents__main-contents__body__text__user" > ${mes.user_name} </div>
+                  <div class = "contents__main-contents__body__text__time"> ${mes.date} </div>
+                  <div class = "contents__main-contents__body__text__message"> ${mes.content} </div>
+                  </div>
+                `
+    return html;
+  }
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    // alert('test_message')
+
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.contents__main-contents__body').append(html)
+      $('.contents__main-contents__body').animate({scrollTop: $('.contents__main-contents__body')[0].scrollHeight}, 'fast')
+      // $('.contents__main-contents__footter__form__button')[0].reset()
+      $('.contents__main-contents__footter__form__button').prop("disabled", false)
+
+    })
+
+    .fail(function(){
+      alert('error');
+    })
+      return false;
+
+    });
+
+
+
+    })
+
+});
+
+
